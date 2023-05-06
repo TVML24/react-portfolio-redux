728012a5d10a236c3f584f60d170c2b3ed169e86
Tim <tvanmookletcher@gmail.com>
2023-05-06 15:08:31 +0930

    initial commit with skeleton code

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Aboutme from './pages/Aboutme';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Aboutme');
+
+  const renderPage = () => {
+    if (currentPage === 'Aboutme') {
+      return <Aboutme />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
